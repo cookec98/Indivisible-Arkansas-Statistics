c94b7acb674253fe389f53aa12efa78dc51062a8
Josh <thomeczekje@hendrix.edu>
2021-11-05 14:01:46 -0500

    contact update

--- IndivisibleArkansasStatistics/Models/Contact.cs
+++ IndivisibleArkansasStatistics/Models/Contact.cs
@@ -5,14 +5,30 @@ namespace IndivisibleArkansasStatistics.Models
 {
     public class Contact
     {
+        //mando
         public string f_name { get; set; }
+
         public string m_name { get; set; }
+
+        //mando
         public string l_name { get; set; }
+
         public string email { get; set; }
+
+        //at least 1 address is mando
         public string m_address { get; set; }
+
         public DateTime DOB { get; set; }
+
         public string p_aff { get; set; }
+
         public string precinct { get; set; }
+
+        public int phone { get; set; }
+
+        public int zip { get; set; }
+
+
 
         //primary
         public int cid { get; set; }
@@ -21,5 +37,8 @@ namespace IndivisibleArkansasStatistics.Models
         public int lid { get; set; }
         public int intid { get; set; }
 
+        public Registration Registration { get; set; }
+        public Log Log { get; set; }
+        public Interest Interest { get; set; }
     }
 }
