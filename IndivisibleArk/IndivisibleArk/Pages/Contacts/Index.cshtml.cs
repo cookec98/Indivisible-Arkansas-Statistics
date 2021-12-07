52930450065a7c17fe14cf290ad52db0b4d6bd29
cookec98 <cookecampbell@gmail.com>
2021-12-07 17:42:23 -0600

    Added Searching By Name And Filtering By Registration Date To Contacts

--- IndivisibleArk/IndivisibleArk/Pages/Contacts/Index.cshtml.cs
+++ IndivisibleArk/IndivisibleArk/Pages/Contacts/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using IndivisibleArk.Data;
@@ -19,13 +20,44 @@ namespace IndivisibleArk.Pages.Contacts
             _context = context;
         }
 
-        public IList<Contact> Contact { get;set; }
-
-        public async Task OnGetAsync()
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public string ContactInterest { get; set; }
+        public IList<Contact> Contacts { get; set; }
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            Contact = await _context.Contact
-                .Include(c => c.Interest)
-                .Include(c => c.Location).ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            CurrentFilter = searchString;
+            IQueryable<Contact> contactsIQ = from c in _context.Contacts
+                                             select c;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                contactsIQ = contactsIQ.Where(c => c.F_name.Contains(searchString)
+                                       || c.L_name.Contains(searchString)
+                                       || c.M_name.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    contactsIQ = contactsIQ.OrderByDescending(c => c.L_name);
+                    break;
+                case "Date":
+                    contactsIQ = contactsIQ.OrderBy(c => c.Registration);
+                    break;
+                case "date_desc":
+                    contactsIQ = contactsIQ.OrderByDescending(c => c.Registration);
+                    break;
+                default:
+                    contactsIQ = contactsIQ.OrderBy(c => c.L_name);
+                    break;
+            }
+
+            Contacts = await contactsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
