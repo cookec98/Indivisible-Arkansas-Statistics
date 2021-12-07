c22e1ecf489f7cc1345dd65c3f0f422b3a1ae63e
Josh <thomeczekje@hendrix.edu>
2021-12-07 14:54:06 -0600

    Interests and Locations display properly

--- IndivisibleArk/IndivisibleArk/Models/Location.cs
+++ IndivisibleArk/IndivisibleArk/Models/Location.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace IndivisibleArk.Models
 {
     public class Location
     {
+        [Display(Name = "Location")]
         public string Loc { get; set; }
 
         //primary
