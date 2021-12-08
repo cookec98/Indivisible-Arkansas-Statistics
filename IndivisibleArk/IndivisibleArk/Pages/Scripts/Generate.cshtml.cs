0c286e216500884b6b7ea9263b56a4c1a4a5456d
cookec98 <cookecampbell@gmail.com>
2021-12-08 16:52:01 -0600

    Added Contacts to the Generate Page

--- IndivisibleArk/IndivisibleArk/Pages/Scripts/Generate.cshtml.cs
+++ IndivisibleArk/IndivisibleArk/Pages/Scripts/Generate.cshtml.cs
@@ -13,17 +13,29 @@ namespace IndivisibleArk.Pages.Scripts
     public class GenerateModel : PageModel
     {
         private readonly IndivisibleArk.Data.ArkContext _context;
-
+        public string CurrentFilter { get; set; }
         public GenerateModel(IndivisibleArk.Data.ArkContext context)
         {
             _context = context;
         }
 
         public IList<Script> Script { get; set; }
-        public async Task OnGetAsync()
+        public IList<Contact> Contact { get; set; }
+        public async Task OnGetAsync(string searchString)
         {
             Script = await _context.Script_1
                 .Include(s => s.Interest).ToListAsync();
+            CurrentFilter = searchString;
+            IQueryable<Contact> contactsIQ = from c in _context.Contacts
+                                             select c;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                contactsIQ = contactsIQ.Where(c => c.F_name.Contains(searchString)
+                                       || c.L_name.Contains(searchString)
+                                       || c.M_name.Contains(searchString));
+            }
+            Contact = await contactsIQ.AsNoTracking().ToListAsync();
         }
+        
     }
 }
